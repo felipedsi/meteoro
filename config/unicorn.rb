60fbc7c0a295a137b5df905ad8a987e6e7d1b213
Felipe Duarte <sduartefelipe@gmail.com>
2017-05-30 23:24:51 -0300

    Config unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 require 'dotenv/load'
 require 'sequel'
 
-worker_processes ENV['WEB_CONCURRENCY'].to_i || 5
-timeout ENV['WEB_TIMEOUT'].to_i || 30000
+worker_processes Integer(ENV['WEB_CONCURRENCY'].to_i || 5)
+timeout Integer(ENV['WEB_TIMEOUT'].to_i || 30000)
 preload_app true
 
 before_fork do |server, worker|
